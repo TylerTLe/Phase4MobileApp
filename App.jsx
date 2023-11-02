955519c8b2e9ae35e0b47754b5bccc5874e1a5da
TylerTLe <tyler.le@edu.sait.ca>
2023-11-02 13:13:15 -0600

    updates files

---
+++ App.jsx
@@ -0,0 +1,75 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import 'react-native-gesture-handler';
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  StatusBar
+} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+const Tab = createBottomTabNavigator();
+export default function App(){
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        {
+
+        }
+        <Tab.Screen name="Home" component={HomeScreen}></Tab.Screen>
+        <Tab.Screen name="Stats" component={StatsScreen}></Tab.Screen>
+        <Tab.Screen name="Settings" component={SettingsScreen}></Tab.Screen>
+        <Tab.Screen name="Profile" component={ProfileScreen}></Tab.Screen>
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent:'center',
+  },
+});
+
+function HomeScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>HOME</Text>
+    </View>
+  );
+}
+
+function StatsScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>STATS!</Text>
+    </View>
+  );
+}
+
+function SettingsScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Settings!</Text>
+    </View>
+  );
+}
+
+function ProfileScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>PROFILES</Text>
+    </View>
+  );
+}
